2137aac6ee727e74596b08ada9dd9c5f81f745b6
John Matthew Griffis <grifj153@newschool.edu>
2012-12-04 11:29:56 -0500

    Drew a sphere.

--- Fall2012final/src/testApp.cpp
+++ Fall2012final/src/testApp.cpp
@@ -2,60 +2,66 @@
 
 //--------------------------------------------------------------
 void testApp::setup(){
-
+    
+    ofSetCircleResolution(60);
+    ofEnableSmoothing();
+    ofSetFrameRate(60);
+    ofSetVerticalSync(true);
+    ofBackground(0);
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-
+    
+    ofSphere(ofGetWidth()/2, ofGetHeight()/2, 0, 50);
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
-
+void testApp::dragEvent(ofDragInfo dragInfo){
+    
 }
